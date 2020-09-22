d37e71495791432dbdd5984f174d3f4823f02d8d
Andre <falconi879@gmail.com>
2020-09-22 10:29:32 -0300

    Integrando a Classe Dominio com a REST (resource)

--- src/main/java/br/com/jawebsites/vendas/resources/CategoriaResource.java
+++ src/main/java/br/com/jawebsites/vendas/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package br.com.jawebsites.vendas.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.jawebsites.vendas.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,8 +15,15 @@ public class CategoriaResource {
 	
 	
 	@GetMapping
-	public String listar() {
-		return "Rest esta funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria categoria = new Categoria(1, "Informatica");
+		Categoria categoria2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(categoria);
+		lista.add(categoria2);
+		return lista;
 	}
 
 }
