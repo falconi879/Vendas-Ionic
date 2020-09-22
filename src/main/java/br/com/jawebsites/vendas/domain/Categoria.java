6d304b6351b4586e4ce147433d6d3463a54908e7
Andre <falconi879@gmail.com>
2020-09-22 11:21:40 -0300

    Configuração do banco de teste h2 + anotaçoes para criação da tabela Categoria

--- src/main/java/br/com/jawebsites/vendas/domain/Categoria.java
+++ src/main/java/br/com/jawebsites/vendas/domain/Categoria.java
@@ -2,12 +2,17 @@ package br.com.jawebsites.vendas.domain;
 
 import java.io.Serializable;
 
-import com.sun.source.doctree.SerialDataTree;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Categoria implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
