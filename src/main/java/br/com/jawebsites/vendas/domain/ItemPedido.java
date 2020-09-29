dc0e3795cc3d90edc6b7611fe3a6e0f8151402f7
Andre <falconi879@gmail.com>
2020-09-29 18:59:24 -0300

    Anonações para criação da tabela ItemPedido no banco de dados

--- src/main/java/br/com/jawebsites/vendas/domain/ItemPedido.java
+++ src/main/java/br/com/jawebsites/vendas/domain/ItemPedido.java
@@ -2,9 +2,14 @@ package br.com.jawebsites.vendas.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@EmbeddedId
 	private IntemPedidoPK id = new IntemPedidoPK(); 	
 	private Double desconto;
 	private Integer quantidade;
